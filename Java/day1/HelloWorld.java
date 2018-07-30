118a94e02c8462059290b384eb74747a7d06defa
shine <jyshine3@gmail.com>
2018-07-30 23:24:51 +0900

    I need to practice

---
+++ Java/day1/HelloWorld.java
@@ -0,0 +1,6 @@
+// 실행은 ctrl + f11로 하면 된다 
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+}
---
+++ Java/day1/TestPerson.java
@@ -0,0 +1,11 @@
+// TestPerson.java 로 저장 
+// 메인메서드를 정의한 클래스 
+public class TestPerson{
+     	//메인메서드,프로그램의 시작점, JVM이 가장 먼저 실행하는 메서드
+	     public static void main(String args[]){
+		//Person 객체 생성 
+		Person p=new Person();
+		//Person 객체 메서드 호출 
+		p.printName();
+	     }	
+}
